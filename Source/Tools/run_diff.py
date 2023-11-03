27634d8b29f8e0aaf4438a1dcec7bc545f1cc13b
unknown <lvadmin@apetersoMeasLinkDev.amer.corp.natinst.com>
2023-11-03 13:34:24 -0500

    Delineate change types (modify vs add) among the lv files to diff.  Improve logging

--- Source/Tools/run_diff.py
+++ Source/Tools/run_diff.py
@@ -15,13 +15,20 @@ _logger.addHandler(handler)
 
 def main():
     # TODO: recieve target branch name as param from workflow
+    target_branch = "origin/main"
+
     # Returns a set of tuples of changed VIs: 'A' for Add, 'M' for Modified
-    diffs = list(get_changed_labview_files("origin/main"))
-    print(diffs)
-    
+    changed_labview_files = list(get_changed_labview_files(target_branch))
+
+    for change_type, filename in changed_labview_files:
+        if change_type == 'A':
+            _logger.debug(f"Added file {filename}")
+        if change_type == 'M':
+            _logger.debug(f"Modified file {filename}")
+
     # Temporarily disable copy of main
-    # temp_directory = export_repo("origin/main")
-    # print(temp_directory)
+    # temp_directory = export_repo(target_branch)
+    # _logger.debug(temp_directory)
 
     return_code = run_full_diff()
     sys.exit(return_code)
@@ -32,7 +39,7 @@ def run_full_diff():
     diff_vi = os.path.join(tools_directory , "run_diff.vi")
     _logger.debug(f"Launching {diff_vi}.")
     test_result = subprocess.run(["LabVIEWCLI", "-OperationName", "RunVI", "-VIPath", os.path.normpath(diff_vi)], capture_output= True)
-    
+
     formatted_stdout = test_result.stdout.decode().replace('\r\n','\n').strip()
     _logger.debug(formatted_stdout)
     if(test_result.returncode != 0):
