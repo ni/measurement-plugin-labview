d64bd7cf88c308152b0d773436443e8fa7cf2553
Adam Peterson <adam.peterson@ni.com>
2023-11-08 18:07:28 -0600

    Clean up and streamline python logic in main script.

--- Source/Tools/run_diff.py
+++ Source/Tools/run_diff.py
@@ -1,10 +1,12 @@
 
 import logging
 import os
+import shutil
 import subprocess
 import sys
+import tempfile
+from pathlib import Path
 from labview_diff import get_changed_labview_files
-from labview_diff import export_repo
 
 _logger = logging.getLogger(__name__)
 _logger.setLevel(logging.DEBUG)
@@ -13,15 +15,19 @@ handler = logging.StreamHandler(sys.stdout)
 handler.setLevel(logging.DEBUG)
 _logger.addHandler(handler)
 
+
 def run_full_diff():
     # TODO: recieve target branch name as param from workflow
     target_branch = "origin/main"
 
     (added_labview_files, modified_labview_files) = get_changed_labview_files(target_branch)
 
-    target_snapshot_directory = export_repo(target_branch)
+    tools_directory = Path(os.path.dirname(__file__))
+    source_directory = tools_directory.parent
+    repo_root_directory = source_directory.parent
 
-    tools_directory = os.path.abspath(os.path.dirname(__file__))
+    target_snapshot_directory = checkout_target_from_repo(repo_root_directory, target_branch)
+
     diff_vi = os.path.join(tools_directory , "run_diff.vi")
     _logger.debug(f"Launching {diff_vi}.")
 
@@ -46,6 +52,16 @@ def run_full_diff():
 def main():
     return_code = run_full_diff()
     sys.exit(return_code)
+
+
+def checkout_target_from_repo(repo_root_directory, target_branch):
+    temp_directory = tempfile.TemporaryDirectory()
+    print(temp_directory)
+
+    shutil.copytree(os.path.join(repo_root_directory, ".git"), os.path.join(temp_directory.name, ".git"))
+    subprocess.check_call(["git", "checkout", "-f", target_branch], cwd=temp_directory.name)
+
+    return (temp_directory)
 
 
 main()
