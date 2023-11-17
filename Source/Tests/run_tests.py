16a92812b318a7159919bc367929bf36fa4a3d90
Mounika Battu <mounika.battu@ni.com>
2023-11-17 15:13:07 +0530

    Feat: Include Port number in LabVIEWCLI

--- Source/Tests/run_tests.py
+++ Source/Tests/run_tests.py
@@ -13,18 +13,18 @@ handler.setLevel(logging.DEBUG)
 _logger.addHandler(handler)
 
 def main():
-    labview_path = _parse_command_line_args()
-    return_code = run_all_tests(labview_path)
+    labview_path, port_number = _parse_command_line_args()
+    return_code = run_all_tests(labview_path, port_number)
     sys.exit(return_code)
 
 
-def run_all_tests(labview_path: str):
+def run_all_tests(labview_path: str, port_number: str):
     test_directory = os.path.abspath(os.path.dirname(__file__))
     test_runner_vi = os.path.join(test_directory , "run_tests.vi")
     _logger.debug(f"Launching {test_runner_vi}.")
     kwargs = ["LabVIEWCLI", "-OperationName", "RunVI", "-VIPath", os.path.normpath(test_runner_vi)]
     if labview_path:
-        kwargs.extend(["-LabVIEWPath", labview_path])
+        kwargs.extend(["-LabVIEWPath", labview_path, "-PortNumber", port_number])
     test_result = subprocess.run(kwargs, capture_output= True)
     
     formatted_stdout = test_result.stdout.decode().replace('\r\n','\n').strip()
@@ -38,10 +38,11 @@ def run_all_tests(labview_path: str):
 
 def _parse_command_line_args():
     parser = argparse.ArgumentParser(description="Run LabVIEW tests using LabVIEWCLI")
-    parser.add_argument("labview_path", type=str, help="Path to the LabVIEW executable", nargs='?', default=None)
+    parser.add_argument("--labview_path", type=str, help="Path to the LabVIEW executable", nargs='?', default=None)
+    parser.add_argument("--port_number", type=str, help="TCP/IP port number of LabVIEW executable", nargs='?', default="3363")
 
     args = parser.parse_args()
-    return args.labview_path
+    return args.labview_path, args.port_number
 
 
 main()
